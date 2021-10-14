e501ba17d50d062cff606e02a455f72787e13c87
johannespanzer <johannes-panzer@gmx.de>
2021-10-14 22:41:01 +0200

    push

--- public/tests/test.php
+++ public/tests/test.php
@@ -4,6 +4,7 @@ require_once __DIR__.'/../../vendor/autoload.php';
 $receivedData = json_decode(file_get_contents("php://input"), true);
 echo json_encode([
     'success' => true,
-    'erfolg' => 'jup'
+    'erfolg' => 'jup',
+    'data' => $receivedData
 ], JSON_THROW_ON_ERROR);
 exit();
